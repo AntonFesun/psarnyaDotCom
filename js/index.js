d064b0862b978935bdf5da1233b91a1c99513784
AntonFesun <fesunanton@gmail.com>
2018-08-14 21:47:48 +0300

    landing_page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var fullPage = new fullpage('#fullpage', {
+    autoScrolling: true,
+    scrollHorizontally: true,
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+    menu: '#myMenu',
+    loopBottom: true
+});
+
