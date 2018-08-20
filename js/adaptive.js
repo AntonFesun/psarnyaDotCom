88db94fbfc70e960dc09386144554e9aceaf040c
AntonFesun <fesunanton@gmail.com>
2018-08-20 11:46:40 +0300

    menu

--- js/adaptive.js
+++ js/adaptive.js
@@ -1,12 +1,16 @@
 let doc = document,
     myMenu = doc.getElementById('myMenu'),
-    buttonMenu = doc.getElementById('open-menu');
+    buttonMenu = doc.getElementById('open-menu'),
+    background = doc.getElementById('fullpage');
 
 
 buttonMenu.onclick = function adaptation() {
     if(myMenu.style.display === '') {
         myMenu.style.display = 'block';
+        background.style.filter = 'blur(25px) brightness(10%)';
+
     } else {
         myMenu.style.display = '';
+        background.style.filter = 'none';
     }
 };
